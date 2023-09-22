5fb52cbe5c72830e4152efdbac45b337dc73d07d
saroj kumar sah <sarojahah152@gmail.com>
2023-09-22 18:05:09 +0545

    qn6

--- TaskQn6.java
+++ TaskQn6.java
@@ -1,3 +1,27 @@
+// 6. Write a program that reads the width and generates a corresponding triangle of *. For example, if width = 5, output is:
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// *****
+import java.util.Scanner;
+
 public class TaskQn6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter Width");
+        int width = scanner.nextInt();
+        for (int i=1;i<=width;i++) {
+            for (int j=0;j<i;j++) {
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
+    }
     
 }
