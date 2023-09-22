9a019834d4a843c30d633412fe79703d9b2c140f
saroj kumar sah <sarojahah152@gmail.com>
2023-09-22 17:37:23 +0545

    Qn4

--- TaskQn4.java
+++ TaskQn4.java
@@ -1,3 +1,20 @@
+// 4. Write a program that reads the width and generates a corresponding square of *. For example, if width = 5, output is:
+
+// *****
+
+// *****
+
+// *****
+
+// *****
+
+// *****
+
 public class TaskQn4 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5;i++) {
+            System.out.println("*****");
+        }
+    }
     
 }
