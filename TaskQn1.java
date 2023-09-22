f8fa2cb6db9b23e408283747c70f4ac9a1076fe5
saroj kumar sah <sarojahah152@gmail.com>
2023-09-22 17:26:28 +0545

    Qn1

---
+++ TaskQn1.java
@@ -0,0 +1,9 @@
+// 1. Write a program that asks your name and prints “Hello your name” five times. Use a loop.
+// 
+public class TaskQn1{
+    public static void main(String[] args) {
+        for (int i = 0; i <5;i++) {
+            System.out.println("Hello Saroj kumar sah");
+        }
+    }
+}
