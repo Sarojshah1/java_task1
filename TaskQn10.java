f17bfd3dc0530dd25e23fab1ee7e32f63793518b
saroj kumar sah <sarojahah152@gmail.com>
2023-09-22 18:09:10 +0545

    qn10

--- TaskQn10.java
+++ TaskQn10.java
@@ -1,3 +1,17 @@
+// 10. Write a program that asks the user for a positive nonzero integer value. The program should use a loop to get the sum of all the integers from 1 up to the number entered. For example, if the user enters 50, the loop will find the sum of 1+2+3+4+….+50.
+import java.util.Scanner;
 public class TaskQn10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num=0;
+        System.out.println("enter a number that you want to sum");
+        int a=scanner.nextInt();
+        for (int i=0; i<=a; i++) {
+            num=num+i;
+
+        }
+        System.out.println("the sum of "+a+" is "+num);
+    }
+    
     
 }
